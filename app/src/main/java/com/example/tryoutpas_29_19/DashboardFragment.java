5e9b5e2c6fcd937d12f8aeb96e4e608d49f371f0
Tayolon <jordan.thomas0811@gmail.com>
2025-05-15 14:18:23 +0700

    JETT

--- app/src/main/java/com/example/tryoutpas_29_19/DashboardFragment.java
+++ app/src/main/java/com/example/tryoutpas_29_19/DashboardFragment.java
@@ -7,7 +7,6 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
-
 public class DashboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
--- app/src/main/java/com/example/tryoutpas_29_19/HomeFragment.java
+++ app/src/main/java/com/example/tryoutpas_29_19/HomeFragment.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
-
 public class HomeFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
